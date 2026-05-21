76a115cbccdb6b626b9a573c4eca2592f72f20ba
Kajol P <kajlir192@gmail.com>
2026-05-20 18:41:35 -0700

    Added play button

--- titlevictory.js
+++ titlevictory.js
@@ -8,22 +8,41 @@ class Title extends Phaser.Scene {
         this.load.image('titleLetters', 'rolypolytitleletters.png');
         this.load.image('titleBackground', 'sluggamebackground.png');
         this.load.image('rolypoly', 'rolypoly.png');
+        this.load.image('playButton', 'playbutton.png');
     }
 
     create() {
         this.titleBackground = this.add.image(400, 300, 'titleBackground');
         this.titleLetters = this.add.image(250, 150, 'titleLetters');
-        this.rolypoly = this.add.image(200, 450, 'rolypoly');
+        this.titleLetters.alpha = 0;
+        this.rolypoly = this.add.image(0, 450, 'rolypoly');
+        this.playButton = this.add.image(600, 300, 'playButton').setInteractive();
+        this.playButton.setScale(0.15);
 
+        this.playButton.on('pointerdown', () => {
+            this.goToScene('gameplayScene');
+        });
+        
         this.titleLetters.setScale(0.1);
-        this.rolypoly.setScale(0.2);    
-    }
+        this.rolypoly.setScale(0.2);  
+
+        this.tweens.add({
+            targets: this.titleLetters,
+            alpha: 1,
+            duration: 1000,
+        });
+
+
+        this.tweens.add({
+            targets: this.rolypoly,
+            x: 200,
+            y: 450,
+            duration: 2000,
+            ease: 'Power2',
+        });    
+}
 
     update() {}
-
-    sceneTransition() {
-        this.cameras.main.fade(1000, 0, 0, 0);
-    }
 }
 
 class GameplayScene extends Phaser.Scene {
@@ -42,7 +61,7 @@ let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: [ Title]
+    scene: [ Title, GameplayScene ]
 }
 
 let game = new Phaser.Game(config);
